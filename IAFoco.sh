5ed7b51f922f144952d1148199ab7a5b7c92b7ad
Figas <matheus.00000@gmail.com>
2025-07-18 09:02:04 +0000

    Atualização automática: 2025-07-18 09:02:04

--- IAFoco.sh
+++ IAFoco.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 # Ativa o ambiente virtual e roda o extract.py
 # Caminho até a pasta do projeto
-#cd /home/figas/Desktop/FocoRadical
+cd /home/figas/Desktop/FocoRadical
 # Ativa o venv
-#source venv/bin/activate
+source venv/bin/activate
 # Executa o script Python
 python3 extract.py
-
