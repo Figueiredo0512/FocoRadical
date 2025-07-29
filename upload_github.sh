034e543438c4360a4c40162cae0d14b5d1d5d0ee
Matheus Figueiredo <matheus.00000@gmail.com>
2025-07-29 19:38:13 +0000

    Atualização automática: 2025-07-29 19:38:13

--- upload_github.sh
+++ upload_github.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 # Caminho do repositório
-
 cd /home/vboxuser/Desktop/FocoRadical
 git status
-#git pull origin main
+
 git add .
+
 git commit -m "Atualização automática: $(date '+%Y-%m-%d %H:%M:%S')"
+
 git push origin main
